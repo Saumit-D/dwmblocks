d32c6de9e795081c0f19d20d34a4a24409b83c24
Saumit Dinesan <justsaumit@protonmail.com>
2022-06-18 21:47:15 +0530

    Updated config.h

--- config.h
+++ config.h
@@ -1,14 +1,15 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 
-/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-  {"",  "nettraf",  1,  16}, 
-  {"",  "volume", 1,  10},
-  {"",  "clock", 5, 1},
-  {"",  "battery",  5, 3},
-  {"",  "internet", 5,  4},
+/*Icon*/	/*Command*/  /*Update Interval*/ /*Update Signal*/
+  {"",      "cpu",       2,     5},
+  {"",      "nettraf",   1,     16}, 
+  {"",      "memory",    5,     8},
+  {"",      "volume",    1,     10},
+  {"",      "clock",     5,     1},
+  {"",      "battery",   5,     3},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " |";
+static char delim[] = "|";
 static unsigned int delimLen = 5;
